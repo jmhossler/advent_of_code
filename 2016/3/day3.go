d5ad60b16ce4d82f9fe773b01f524fb7f9837ae1
jmhossler <john.m.hossler@gmail.com>
2016-12-02 22:11:19 -0600

    Made initial stuff for day3 of 2016

---
+++ 2016/3/day3.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	fmt.Println("Day 3 of Advent of Code 2016")
+
+	data, _ := ioutil.ReadFile("input")
+
+	input := string(data)
+
+}
