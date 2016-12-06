357e86498fc06d14628c25af0eaa4d18e0da05ed
jmhossler <john.m.hossler@gmail.com>
2016-12-05 23:39:45 -0600

    Modified to condense _slightly_

--- 2016/6/day6.go
+++ 2016/6/day6.go
@@ -8,15 +8,11 @@ import (
 
 func main() {
 	fmt.Println("Day 6 of Advent of Code 2016")
+	f, _ := os.Open("input")
 
-	f, err := os.Open("input")
-	check(err)
-
-	var m []map[byte]int
-
-	for i := 0; i < 8; i++ {
-		new_map := make(map[byte]int)
-		m = append(m, new_map)
+	m := [8]map[byte]int{}
+	for i := 0; i < 8; i++ { // init each index of m
+		m[i] = make(map[byte]int)
 	}
 
 	scanner := bufio.NewScanner(f)
@@ -30,40 +26,22 @@ func main() {
 	var text []byte
 	var pt2_text []byte
 	for i := 0; i < 8; i++ {
-		text = append(text, get_highest(m[i]))
-		pt2_text = append(pt2_text, get_lowest(m[i]))
+		text = append(text, get_extreme_key(m[i], func(x, y int) bool { return x > y }, -1))
+		pt2_text = append(pt2_text, get_extreme_key(m[i], func(x, y int) bool { return x < y }, 1))
 	}
 
 	fmt.Printf("Part 1: %s\n", text)
 	fmt.Printf("Part 2: %s\n", pt2_text)
 }
 
-func get_lowest(m map[byte]int) byte {
-	lowest := 10000
-	var store byte
+func get_extreme_key(m map[byte]int, f func(x, y int) bool, parity int) byte {
+	extreme := parity * 1000
+	var key byte
 	for k, v := range m {
-		if v < lowest {
-			lowest = v
-			store = k
+		if f(v, extreme) {
+			extreme = v
+			key = k
 		}
 	}
-	return store
-}
-
-func get_highest(m map[byte]int) byte {
-	var highest int
-	var store byte
-	for k, v := range m {
-		if v > highest {
-			highest = v
-			store = k
-		}
-	}
-	return store
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+	return key
 }
