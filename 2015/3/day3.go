bebfe95e27f2dfeded9c831e140852d321e24101
jmhossler <john.m.hossler@gmail.com>
2016-12-16 11:27:01 -0600

    Finished day 3 in Go for 2015

--- 2015/3/day3.go
+++ 2015/3/day3.go
@@ -37,6 +37,37 @@ func main() {
 	}
 
 	fmt.Printf("Part one: %d unique houses visited\n", len(visited))
+
+	santa_curr := Coord{0, 0}
+	robo_curr := Coord{0, 0}
+	visited = CoordArr{santa_curr}
+	for i := 0; i < len(input); i += 2 {
+		if input[i] == '>' {
+			santa_curr = Coord{santa_curr.x + 1, santa_curr.y}
+		} else if input[i] == '<' {
+			santa_curr = Coord{santa_curr.x - 1, santa_curr.y}
+		} else if input[i] == '^' {
+			santa_curr = Coord{santa_curr.x, santa_curr.y + 1}
+		} else if input[i] == 'v' {
+			santa_curr = Coord{santa_curr.x, santa_curr.y - 1}
+		}
+		if input[i+1] == '>' {
+			robo_curr = Coord{robo_curr.x + 1, robo_curr.y}
+		} else if input[i+1] == '<' {
+			robo_curr = Coord{robo_curr.x - 1, robo_curr.y}
+		} else if input[i+1] == '^' {
+			robo_curr = Coord{robo_curr.x, robo_curr.y + 1}
+		} else if input[i+1] == 'v' {
+			robo_curr = Coord{robo_curr.x, robo_curr.y - 1}
+		}
+		if !visited.contains(santa_curr) {
+			visited = append(visited, santa_curr)
+		}
+		if !visited.contains(robo_curr) {
+			visited = append(visited, robo_curr)
+		}
+	}
+	fmt.Printf("Part two: %d unique houses visited\n", len(visited))
 }
 
 func (v CoordArr) contains(c Coord) bool {
