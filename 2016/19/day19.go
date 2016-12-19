28b5364a5cda53557a457a6e26b6b5c8dea377cc
jmhossler <john.m.hossler@gmail.com>
2016-12-19 15:53:02 -0600

    Fixed part 1

--- 2016/19/day19.go
+++ 2016/19/day19.go
@@ -9,11 +9,11 @@ var numElves = flag.Int("n", 3017957, "Number of elves")
 
 func main() {
 	flag.Parse()
-	//fmt.Println("Day 19 of Advent of Code 2016")
+	fmt.Println("Day 19 of Advent of Code 2016")
 
 	var elf int
-	//elf = getElf(*numElves)
-	//fmt.Printf("Part 1: %d\n", elf)
+	elf = getElf(*numElves)
+	fmt.Printf("Part 1: %d\n", elf)
 
 	elf = getElfPt2(*numElves)
 	fmt.Printf("Part 2: %d elves - %d final\n", *numElves, elf)
@@ -31,21 +31,9 @@ func getElfPt2(numElves int) int {
 }
 
 func getElf(numElves int) int {
-	elves := make([]int, numElves)
-	for i := 0; i < numElves; i++ {
-		elves[i] = i + 1
+	pow := 1
+	for pow*2 < numElves {
+		pow = pow * 2
 	}
-
-	for i := 0; len(elves) > 1; i = (i + 1) % (len(elves) + 1) {
-		//fmt.Println(elves)
-		//fmt.Println(i)
-		if i+1 >= len(elves) {
-			elves = elves[1:]
-		} else if i+2 >= len(elves) {
-			elves = elves[:len(elves)-1]
-		} else {
-			elves = append(elves[:(i+1)%(len(elves))], elves[(i+2)%(len(elves)):]...)
-		}
-	}
-	return elves[0]
+	return 2*(numElves-pow) + 1
 }
