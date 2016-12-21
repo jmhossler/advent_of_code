ce6453daee68dc1fecc3effcbbc2dfa748b3d244
jmhossler <john.m.hossler@gmail.com>
2016-12-21 17:25:03 -0600

    Made solution for part two. Reversed commands instead of guessing approach

--- 2016/21/day21.go
+++ 2016/21/day21.go
@@ -10,6 +10,18 @@ import (
 )
 
 var password = flag.String("p", "abcdefgh", "password to scramble")
+var scrambledPass = flag.String("s", "fbgdceah", "scrambled password")
+
+var reverseMap = map[int]int{
+	1: 0,
+	3: 1,
+	5: 2,
+	7: 3,
+	2: 4,
+	4: 5,
+	6: 6,
+	0: 7,
+}
 
 func main() {
 	flag.Parse()
@@ -20,31 +32,84 @@ func main() {
 	scrambled := scrambler([]byte(*password), input)
 	fmt.Printf("Part 1: %s\n", scrambled)
 
-	permutations := getPermutations("fbgdceah")
-	fmt.Println(permutations)
-	for i := 0; i < len(permutations); i++ {
-		if scrambler(permutations[i], input) == "fbgdceah" {
-			fmt.Printf("Part 2: %s\n", permutations[i])
-			break
-		}
-	}
+	unscrambled := unScramble([]byte(*scrambledPass), input)
+	fmt.Printf("Part 2: %s\n", unscrambled)
 }
 
-func getPermutations(s string) [][]byte {
-	permutations := [][]byte{}
-	for i := 0; i < len(s); i++ {
-		fmt.Println(strings.Replace(s, string(s[i]), "", -1))
-		subPermutations := getPermutations(strings.Replace(s, string(s[i]), "", -1))
-		for _, p := range subPermutations {
-			substring := make([]byte, len(s))
-			substring[0] = s[i]
-			for j, b := range p {
-				substring[j+1] = b
+func unScramble(scrambled []byte, commands []string) string {
+	for i := len(commands) - 1; i >= 0; i-- {
+		fields := strings.Fields(commands[i])
+		switch fields[0] {
+		case "swap":
+			scrambled = swapPass(scrambled, fields)
+		case "rotate":
+			dir := fields[1]
+			var x int
+			if dir == "based" {
+				char := fields[6][0]
+				dir = "left"
+				index := getIndex(scrambled, char)
+				for getIndex(scrambled, char) != reverseMap[index] {
+					scrambled = rotatePassword(scrambled, dir, 1)
+				}
+			} else {
+				if dir == "left" {
+					dir = "right"
+				} else {
+					dir = "left"
+				}
+				x, _ = strconv.Atoi(fields[2])
+				scrambled = rotatePassword(scrambled, dir, x)
 			}
-			permutations = append(permutations, substring)
+		case "reverse":
+			x, _ := strconv.Atoi(fields[2])
+			y, _ := strconv.Atoi(fields[4])
+			scrambled = reversePass(scrambled, x, y)
+		case "move":
+			x, _ := strconv.Atoi(fields[2])
+			y, _ := strconv.Atoi(fields[5])
+			scrambled = movePass(scrambled, y, x)
 		}
 	}
-	return permutations
+	return string(scrambled)
+}
+
+func diff(a, b int) int {
+	result := a - b
+	if result < 0 {
+		return result * -1
+	}
+	return result
+}
+
+func swapPass(p []byte, fields []string) []byte {
+	var x, y byte
+	if fields[1] == "position" {
+		a, _ := strconv.Atoi(fields[2])
+		b, _ := strconv.Atoi(fields[5])
+		x = p[a]
+		y = p[b]
+	} else {
+		x = fields[2][0]
+		y = fields[5][0]
+	}
+	for i := 0; i < len(p); i++ {
+		if p[i] == x {
+			p[i] = y
+		} else if p[i] == y {
+			p[i] = x
+		}
+	}
+	return p
+}
+
+func getIndex(p []byte, c byte) int {
+	for i := 0; i < len(p); i++ {
+		if p[i] == c {
+			return i
+		}
+	}
+	return 0
 }
 
 func scrambler(password []byte, commands []string) string {
@@ -53,36 +118,14 @@ func scrambler(password []byte, commands []string) string {
 		fields := strings.Fields(cmd)
 		switch fields[0] {
 		case "swap":
-			var x, y byte
-			if fields[1] == "position" {
-				a, _ := strconv.Atoi(fields[2])
-				b, _ := strconv.Atoi(fields[5])
-				x = password[a]
-				y = password[b]
-			} else {
-				x = fields[2][0]
-				y = fields[5][0]
-			}
-			for i := 0; i < len(password); i++ {
-				if password[i] == x {
-					password[i] = y
-				} else if password[i] == y {
-					password[i] = x
-				}
-			}
+			password = swapPass(password, fields)
 		case "rotate":
 			dir := fields[1]
 			var x int
 			if dir == "based" {
 				char := fields[6][0]
 				dir = "right"
-				var index int
-				for i := 0; i < len(password); i++ {
-					if password[i] == char {
-						index = i
-					}
-				}
-				x = index
+				x = getIndex(password, char)
 				if x > 3 {
 					x += 1
 				}
@@ -94,19 +137,29 @@ func scrambler(password []byte, commands []string) string {
 		case "reverse":
 			x, _ := strconv.Atoi(fields[2])
 			y, _ := strconv.Atoi(fields[4])
-			for i := 0; i < ((y-x)/2)+1; i++ {
-				password[x+i], password[y-i] = password[y-i], password[x+i]
-			}
+			password = reversePass(password, x, y)
 		case "move":
 			x, _ := strconv.Atoi(fields[2])
 			y, _ := strconv.Atoi(fields[5])
-			char := password[x]
-			password = append(password[:x], password[x+1:]...)
-			password = append(password[:y], append([]byte{char}, password[y:]...)...)
+			password = movePass(password, x, y)
 		}
 		//fmt.Printf("result:\t%s\n", password)
 	}
 	return string(password)
+}
+
+func reversePass(p []byte, x, y int) []byte {
+	for i := 0; i < ((y-x)/2)+1; i++ {
+		p[x+i], p[y-i] = p[y-i], p[x+i]
+	}
+	return p
+}
+
+func movePass(p []byte, x, y int) []byte {
+	char := p[x]
+	p = append(p[:x], p[x+1:]...)
+	p = append(p[:y], append([]byte{char}, p[y:]...)...)
+	return p
 }
 
 func rotatePassword(p []byte, dir string, n int) []byte {
