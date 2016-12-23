b02e158b576dd06e18f8dc3c37f75dba08722bad
jmhossler <john.m.hossler@gmail.com>
2016-12-23 00:52:57 -0600

    Completed day 23

--- 2016/23/day23.go
+++ 2016/23/day23.go
@@ -1,7 +1,127 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var registers = map[string]int{
+	"a": 7,
+	"b": 0,
+	"c": 0,
+	"d": 0,
+}
 
 func main() {
 	fmt.Println("Day 23 of Advent of Code 2016")
+
+	commands := readInput(os.Stdin)
+	var a = make([]string, len(commands))
+	copy(a, commands)
+
+	execute(commands)
+
+	fmt.Printf("Part 1: %d in a\n", registers["a"])
+
+	registers["a"] = 12
+	registers["b"] = 0
+	registers["c"] = 0
+	registers["d"] = 0
+	execute(a)
+
+	fmt.Printf("Part 2: %d in a\n", registers["a"])
+}
+
+func execute(commands []string) {
+	for i := 0; i < len(commands); {
+		/*
+			fmt.Println(commands[i])
+			fmt.Println(registers)
+		*/
+		fields := strings.Fields(commands[i])
+		if fields[0] == "tgl" {
+			x := fields[1]
+			var val int
+			var err error
+			if val, err = strconv.Atoi(x); err != nil {
+				val = registers[x]
+			}
+			//fmt.Println(val)
+			if val+i < len(commands) {
+				commands[i+val] = tgl(commands[i+val])
+			}
+			i++
+		} else {
+			i += step(commands[i])
+		}
+	}
+}
+func tgl(cmd string) string {
+	fields := strings.Fields(cmd)
+	switch len(fields) {
+	case 2:
+		if fields[0] == "inc" {
+			fields[0] = "dec"
+		} else {
+			fields[0] = "inc"
+		}
+	case 3:
+		if fields[0] == "jnz" {
+			fields[0] = "cpy"
+		} else {
+			fields[0] = "jnz"
+		}
+	}
+	return strings.Join(fields, " ")
+}
+
+func step(cmd string) int {
+	fields := strings.Fields(cmd)
+	switch fields[0] {
+	case "cpy":
+		x := fields[1]
+		y := fields[2]
+		if _, ok := registers[y]; ok {
+			if val, err := strconv.Atoi(x); err == nil {
+				registers[y] = val
+			} else {
+				registers[y] = registers[x]
+			}
+		}
+	case "inc":
+		if _, ok := registers[fields[1]]; ok {
+			registers[fields[1]]++
+		}
+	case "dec":
+		if _, ok := registers[fields[1]]; ok {
+			registers[fields[1]]--
+		}
+	case "jnz":
+		x, err := strconv.Atoi(fields[1])
+		if err != nil {
+			x = registers[fields[1]]
+		}
+		y, err := strconv.Atoi(fields[2])
+		if err != nil {
+			y = registers[fields[2]]
+		}
+		//fmt.Printf("x: %d\ty: %d\n", x, y)
+		if x != 0 {
+			return y
+		}
+		return 1
+	}
+	return 1
+}
+
+func readInput(f *os.File) []string {
+	scanner := bufio.NewScanner(f)
+	data := []string{}
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+	return data
 }
