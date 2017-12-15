f69f0c133c2905cb8936b87a3add95966f7c64dd
John Hossler <john.m.hossler@adtran.com>
2017-12-14 23:28:39 -0600

    okay well I did it and it runs but don't ask about speed

--- 2017/15/solution.py
+++ 2017/15/solution.py
@@ -1,22 +1,36 @@
-a_val = 883
 b_val = 879
 
 # test
-a_val = 65
-b_val = 8921
+#b_val = 8921
 
 a_factor = 16807
 b_factor = 48271
 modulo_val = 2147483647
 
+def a_generator(n):
+    a_val = 883
+    count = 0
+    while count < n:
+        a_val = (a_val * a_factor) % modulo_val
+        if a_val % 4 == 0:
+            yield a_val
+            count += 1
+
+def b_generator(n):
+    b_val = 879
+    count = 0
+    while count < n:
+        b_val = (b_val * b_factor) % modulo_val
+        if b_val % 8 == 0:
+            yield b_val
+            count += 1
+
+
 match = 0
-iterations = 40000000
-for i in range(0, iterations):
-    a_val = (a_val * a_factor) % modulo_val
-    b_val = (b_val * b_factor) % modulo_val
+iterations = 5000000
+for a_val, b_val in zip(a_generator(iterations), b_generator(iterations)):
     a_bits = ('{:64b}'.format(a_val))[-16:]
     b_bits = ('{:64b}'.format(b_val))[-16:]
-
     if a_bits == b_bits:
         match += 1
 
