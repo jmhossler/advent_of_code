8a010c651d368e48d04b4c5dcaa75aefa8aec493
John Hossler <john.m.hossler@adtran.com>
2017-12-16 23:32:50 -0600

    let's try linked lists!

--- 2017/17/solution.py
+++ 2017/17/solution.py
@@ -5,11 +5,47 @@ with open(sys.argv[1], 'r') as f: steps = int(f.read())
 buf = [0]
 curr_index = 0
 
-for i in range(1, 50000001):
-    curr_index = (curr_index + steps) % len(buf)
-    next_val = (curr_index + 1)
-    buf = buf[:next_val] + [i] + buf[next_val:]
-    curr_index = next_val
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
-print(buf[curr_index+1])
-print(buf[buf.index(0)+1])
+    def set_next(self, node):
+        self.next = node
+
+    def get_val(self):
+        return self.val
+
+class ll:
+    def __init__(self, val):
+        self.head = Node(val)
+        self.head.set_next(self.head)
+        self.zero = self.head
+        self.length = 1
+
+    def insert(self, val, index):
+        new_node = Node(val)
+
+        curr = 0
+        probe = self.head
+        index = index % self.length
+        while curr < index:
+            probe = probe.next
+            curr += 1
+        next_node = probe.next
+        probe.set_next(new_node)
+        new_node.set_next(next_node)
+
+        self.length += 1
+        self.head = new_node
+
+    def get_next(self):
+        return self.head.next.get_val()
+
+buff = ll(0)
+
+for i in range(1, 50000001):
+    buff.insert(i, steps)
+
+print(buff.get_next())
+print(buff.zero.next.get_val())
