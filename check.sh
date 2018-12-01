9645cce2886c6abc01ca894e8c381f141724bf22
John Hossler <john.m.hossler@gmail.com>
2018-12-01 12:07:57 -0600

    chore(env): extract session details into private env file and remove unused file

--- check.sh
+++
@@ -1,2 +0,0 @@
-pylint 2018/*/*.py --disable=C0111,C0103,E0401,R1710
-pytest 2018/
--- get_input.sh
+++ get_input.sh
@@ -2,4 +2,4 @@
 
 mkdir -p 2018/$1
 
-curl -H "Cookie: session=53616c7465645f5f449edbfe74d47c955bade8432ee5e06da47c0067f7a228922c7d63296e4f7f1e95bd99c2adaefaa1" "https://adventofcode.com/2018/day/$1/input" > 2018/$1/input
+curl -H "Cookie: session=${SESSION_COOKIE}" "https://adventofcode.com/2018/day/$1/input" > 2018/$1/input
