e4ae8c81f11c51343e92c58427de39c1f793da01
John Hossler <john.m.hossler@gmail.com>
2018-12-02 13:55:49 -0600

    chore(2018_2): refactor for clarity

--- 2018/2/solution2.py
+++ 2018/2/solution2.py
@@ -1,25 +1,32 @@
+from itertools import combinations
+
+
 def get_checksum(box_ids):
-    return (len([box_id for box_id in box_ids if contains_matching(2, box_id)])
-            * len([box_id for box_id in box_ids if contains_matching(3, box_id)]))
+    return count_matching(2, box_ids) * count_matching(3, box_ids)
+
+
+def count_matching(n, box_ids):
+    return len([box_id for box_id in box_ids if contains_matching(n, box_id)])
 
 
 def contains_matching(n, box_id):
     letters = {c: box_id.count(c) for c in set(c for c in box_id)}
-    return any([True for letter in letters if letters[letter] == n])
+    return any(True for letter in letters if letters[letter] == n)
 
 
 def get_common_letters(box_ids):
-    for i in range(0, len(box_ids)):
-        for j in range(i, len(box_ids)):
-            if differences(box_ids[i], box_ids[j]) == 1:
-                return remove_difference(box_ids[i], box_ids[j])
-    return None
+    return next(remove_difference(a, b)
+                for a, b in combinations(box_ids, 2)
+                if count_differences(a, b) == 1)
 
-def differences(a, b):
+
+def count_differences(a, b):
     return sum(1 for i, j in zip(a, b) if i != j)
+
 
 def remove_difference(a, b):
     return ''.join(i for i, j in zip(a, b) if i == j)
+
 
 if __name__ == '__main__':
     with open('input') as input_file:
