403c90eac8ae22e9332903ee4fd01c09a79b4101
John Hossler <john.m.hossler@gmail.com>
2018-12-01 20:45:58 -0600

    chore(2018_1): cleanup find_duplicate_frequency

--- 2018/1/solution.py
+++ 2018/1/solution.py
@@ -6,11 +6,9 @@ def get_total_frequency(frequency_deltas):
 
 def find_duplicate_frequency(frequency_deltas):
     previous_frequencies = {0}
-    for frequency in accumulate(cycle(frequency_deltas)):
-        print(frequency)
-        if frequency in previous_frequencies:
-            return frequency
-        previous_frequencies.add(frequency)
+    return next(frequency
+                for frequency in accumulate(cycle(frequency_deltas))
+                if frequency in previous_frequencies or previous_frequencies.add(frequency))
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
