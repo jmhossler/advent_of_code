24d3de575e41a72b2a26e9243a00708a0a9a79da
John Hossler <john.m.hossler@gmail.com>
2018-12-02 19:48:00 -0600

    chore(scripts): move venv location and fix get_input script to work for next year

--- get_input.sh
+++ get_input.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mkdir -p ${AOC_YEAR}
+mkdir -p ${AOC_YEAR}/input
 
 curl -H "Cookie: session=${SESSION_COOKIE}" "https://adventofcode.com/${AOC_YEAR}/day/$1/input" > ${AOC_YEAR}/input/input$1.txt
-
-cd ${AOC_YEAR}
