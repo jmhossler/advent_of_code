c272b80080455b47e72d5e86442f4ac9dfc28542
John Hossler <john@tangotango.net>
2021-12-01 21:21:50 -0800

    refactor: unify the handle_instruction function between the two parts, just use pattern matching to get around the extra obstacle

--- 2021/solutions/day2.exs
+++ 2021/solutions/day2.exs
@@ -4,11 +4,13 @@ defmodule DayTwo do
     IO.puts("Day Two (AOC)")
     {:ok, instructions} = get_instructions(input_file)
 
-    %{horizontal_position: ending_horizontal_position, depth: ending_depth} = Enum.reduce(instructions, %{horizontal_position: 0, depth: 0}, &handle_instruction/2)
+    %{horizontal_position: ending_horizontal_position, depth: ending_depth} =
+      Enum.reduce(instructions, %{horizontal_position: 0, depth: 0}, &handle_instruction/2)
 
     IO.puts("Part one: #{ending_horizontal_position * ending_depth}")
 
-    %{horizontal_position: ending_horizontal_position, depth: ending_depth} = Enum.reduce(instructions, %{horizontal_position: 0, depth: 0, aim: 0}, &handle_instruction_pt2/2)
+    %{horizontal_position: ending_horizontal_position, depth: ending_depth} =
+      Enum.reduce(instructions, %{horizontal_position: 0, depth: 0, aim: 0}, &handle_instruction/2)
 
     IO.puts("Part two: #{ending_horizontal_position * ending_depth}")
   end
@@ -20,33 +22,35 @@ defmodule DayTwo do
          contents
          |> String.split("\n", trim: true)
          |> Enum.map(&String.split(&1, " ", trim: true))
-         |> Enum.map(fn [direction, string_magnitude] -> [direction, String.to_integer(string_magnitude)] end)}
+         |> Enum.map(fn [direction, string_magnitude] ->
+           [direction, String.to_integer(string_magnitude)]
+         end)}
       error -> error
     end
+  end
+
+  defp handle_instruction(["forward", magnitude], %{horizontal_position: x, depth: y, aim: z}) do
+    %{horizontal_position: x + magnitude, depth: y + magnitude * z, aim: z}
   end
 
   defp handle_instruction(["forward", magnitude], %{horizontal_position: x, depth: y}) do
     %{horizontal_position: x + magnitude, depth: y}
   end
 
+  defp handle_instruction(["up", magnitude], %{horizontal_position: x, depth: y, aim: z}) do
+    %{horizontal_position: x, depth: y, aim: z - magnitude}
+  end
+
   defp handle_instruction(["up", magnitude], %{horizontal_position: x, depth: y}) do
     %{horizontal_position: x, depth: y - magnitude}
   end
 
+  defp handle_instruction(["down", magnitude], %{horizontal_position: x, depth: y, aim: z}) do
+    %{horizontal_position: x, depth: y, aim: z + magnitude}
+  end
+
   defp handle_instruction(["down", magnitude], %{horizontal_position: x, depth: y}) do
     %{horizontal_position: x, depth: y + magnitude}
-  end
-
-  defp handle_instruction_pt2(["forward", magnitude], %{horizontal_position: x, depth: y, aim: z}) do
-    %{horizontal_position: x + magnitude, depth: y + magnitude * z, aim: z}
-  end
-
-  defp handle_instruction_pt2(["up", magnitude], %{horizontal_position: x, depth: y, aim: z}) do
-    %{horizontal_position: x, depth: y, aim: z - magnitude}
-  end
-
-  defp handle_instruction_pt2(["down", magnitude], %{horizontal_position: x, depth: y, aim: z}) do
-    %{horizontal_position: x, depth: y, aim: z + magnitude}
   end
 end
 
